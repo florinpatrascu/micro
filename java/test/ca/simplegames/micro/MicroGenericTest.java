960bde96a494dedd925ac9d99dfc0e2e7fd6c49f
Florin T.PATRASCU <florin.patrascu@gmail.com>
2013-01-06 14:06:24 -0500

    added some more basic tests for validating the functionality of the static and dynamic content support

---
+++ java/test/ca/simplegames/micro/MicroGenericTest.java
@@ -0,0 +1,110 @@
+package ca.simplegames.micro;
+
+import org.jrack.Context;
+import org.jrack.Rack;
+import org.jrack.RackResponse;
+import org.jrack.context.MapContext;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import java.net.URLEncoder;
+import java.util.Collections;
+
+//@RunWith(value = Suite.class)
+//@Suite.SuiteClasses(value = {MicroGenericTest.class})
+//@RunWith(OrderedRunner.class)
+//@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+
+/**
+ * Micro Tester.
+ *
+ * @author <a href="mailto:florin.patrascu@gmail.com">Florin T.PATRASCU</a>
+ * @version 1.0
+ * @since <pre>Jan 1, 2013</pre>
+ */
+public class MicroGenericTest {
+    static Micro micro;
+
+
+    @BeforeClass
+    public static void setup() throws Exception {
+        micro = new Micro("files", null, "../../lib", "index.html");
+        Assert.assertNotNull(micro);
+        Assert.assertNotNull("Micro 'site' initialization failed", micro.getSite().getWebInfPath());
+        Assert.assertTrue("Micro is not pointing to the correct test web app",
+                micro.getSite().getWebInfPath().getAbsolutePath().contains("files/WEB-INF"));
+        Assert.assertTrue("Micro test web app is not properly defined",
+                micro.getSite().getWebInfPath().exists());
+    }
+
+
+    /**
+     * testing the welcome page
+     * <p/>
+     * Method: call(Context<String> input)
+     */
+    @Test
+    public void testCall() throws Exception {
+        Context<String> input = new MapContext<String>()
+                .with(Rack.REQUEST_METHOD, "GET")
+                .with(Rack.PATH_INFO, "/index.html");
+
+        RackResponse response = micro.call(input);
+        Assert.assertTrue(RackResponse.getBodyAsString(response).contains("hello"));
+    }
+
+    /**
+     * test if the result is properly localized; using the rack locale
+     * <p/>
+     * Method: testCallWithContextLocale(Context<String> input)
+     */
+    @Test
+    public void testCallWithContextLocale() throws Exception {
+        Context<String> input = new MapContext<String>()
+                .with(Rack.REQUEST_METHOD, "GET")
+                .with(Rack.PATH_INFO, "/index.html")
+                .with(Rack.RACK_BROWSER_LOCALE, "de");
+
+        RackResponse response = micro.call(input);
+        Assert.assertTrue("Context based localization failed",
+                RackResponse.getBodyAsString(response).contains("Grüß Gott!"));
+    }
+
+
+    /**
+     * test static content (binary) served from a dynamic repository, using the support of
+     * {@link ca.simplegames.micro.controllers.BinaryContent}
+     *
+     * @throws Exception
+     */
+    @Test
+    public void testBinaryContent() throws Exception {
+        Context<String> input = new MapContext<String>()
+                .with(Rack.REQUEST_METHOD, "GET")
+                .with(Rack.PATH_INFO, "/micro-logo.png");
+
+        RackResponse response = micro.call(input);
+        Assert.assertTrue("Can't load binary content from a dynamic repository",
+                RackResponse.getBodyAsBytes(response).length == 6898);
+    }
+
+    /**
+     * test dynamic content produced by a controller
+     *
+     * @throws Exception
+     */
+    @Test
+    public void testSomeDynamicContent() throws Exception {
+        Context<String> input = new MapContext<String>()
+                .with(Rack.REQUEST_METHOD, "GET")
+                .with(Rack.PATH_INFO, "/result.html")
+                .with(Globals.PARAMS, Collections.singletonMap("exp",
+                        new String[]{URLEncoder.encode("2+2", Globals.UTF8)}));
+
+
+        RackResponse response = micro.call(input);
+        Assert.assertTrue("Binary content rendering issues",
+                RackResponse.getBodyAsString(response).contains("=4"));
+    }
+}
