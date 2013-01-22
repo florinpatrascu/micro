352c361a6c4f239e8e32ee47b5740bac41967a2f
Florin T.PATRASCU <florin.patrascu@gmail.com>
2013-01-21 20:15:20 -0500

    v02 and no more bloated Jetty, back to cool

--- java/src/ca/simplegames/micro/Globals.java
+++ java/src/ca/simplegames/micro/Globals.java
@@ -22,7 +22,7 @@ package ca.simplegames.micro;
  */
 public class Globals {
     public static final String FRAMEWORK_NAME = "micro";
-    public static final String VERSION = "0.1";
+    public static final String VERSION = "0.2";
     public static final String EMPTY_STRING = "";
     public static final String MICRO = "micro_";
     public static final String UTF8 = "UTF-8";
