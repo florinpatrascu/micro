4f8999245e59fb91517189cc0c281d8e2843a0cc
Florin T.PATRASCU <florin.patrascu@gmail.com>
2013-03-29 11:37:54 -0400

    added the WEB-INF/lib/*.jar to the classpath, when using the run.sh script

--- bin/templates/simple/run.sh
+++ bin/templates/simple/run.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
-libs=$( echo ${MICRO_HOME}/lib/*.jar ${MICRO_HOME}/dist/*.jar . | sed 's/ /:/g')
+libs=$( echo ${MICRO_HOME}/lib/*.jar ${MICRO_HOME}/dist/*.jar WEB-INF/lib/*.jar . | sed 's/ /:/g')
 
 # Start Micro with the embedded Jetty server.
-OPT='-Dnet.sf.ehcache.skipUpdateCheck=true -Xmx128m -Xss512k -XX:+UseCompressedOops'
+# DEBUG="-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5005"
+
+OPT="-Dnet.sf.ehcache.skipUpdateCheck=true -Xmx128m -Xss512k -XX:+UseCompressedOops"
 java $OPT -cp "$libs:WEB-INF/classes" ca.simplegames.micro.WebServer . 8080 $1 $2 $3 $4 $5 $6
