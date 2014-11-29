4b25aabee86bc47983198949d37c4a071f52df41
Florin T.PATRASCU <florin.patrascu@gmail.com>
2014-11-29 17:15:36 -0500

    removed an incomplete example

--- examples/blog/WEB-INF/models/Article.java
+++
@@ -1,8 +0,0 @@
-import org.javalite.activejdbc.Model;
-
-public class Article extends Model {
-    static {
-        validatePresenceOf("title", "summary", "author");
-        validatePresenceOf("author").message("Author must be provided");
-    }
-}
