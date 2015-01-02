28ad5f40c0913f6e1c28979d0f54b65e9b36b4d6
Florin T.PATRASCU <florin.patrascu@gmail.com>
2015-01-02 15:55:41 -0500

    using Jetty 9 as the embedded server

--- micro-core/src/main/java/ca/simplegames/micro/WebServer.java
+++ micro-core/src/main/java/ca/simplegames/micro/WebServer.java
@@ -16,13 +16,13 @@
 
 package ca.simplegames.micro;
 
-import org.mortbay.jetty.Connector;
-import org.mortbay.jetty.Server;
-import org.mortbay.jetty.nio.SelectChannelConnector;
-import org.mortbay.jetty.servlet.ServletHandler;
-import org.mortbay.jetty.webapp.WebAppContext;
-import org.mortbay.thread.QueuedThreadPool;
-import org.mortbay.xml.XmlConfiguration;
+import org.eclipse.jetty.server.Connector;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.ServerConnector;
+import org.eclipse.jetty.servlet.ServletHandler;
+import org.eclipse.jetty.util.thread.QueuedThreadPool;
+import org.eclipse.jetty.webapp.WebAppContext;
+import org.eclipse.jetty.xml.XmlConfiguration;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -36,62 +36,61 @@ import java.io.FileInputStream;
  * @since $Revision$ (created: 2013-01-11 7:28 PM)
  */
 public class WebServer {
-    protected static final Logger log = LoggerFactory.getLogger(WebServer.class);
-    public static final String PORT = "PORT";
+  protected static final Logger log = LoggerFactory.getLogger(WebServer.class);
+  public static final String PORT = "PORT";
+  public static int DEFAULT_MAX_THREADS = 20;
 
-    /**
-     * Will start the embedded Jetty server and will initialize the Micro web app. The web app is a
-     * standard exploded .war structure
-     *
-     * @param args expecting the path to the Micro web app and optionally a port number
-     */
-    public static void main(String[] args) throws Exception {
+  /**
+   * Will start the embedded Jetty server and will initialize the Micro web app. The web app is a
+   * standard exploded .war structure
+   *
+   * @param args expecting the path to the Micro web app and optionally a port number
+   */
+  public static void main(String[] args) throws Exception {
 
-        if (args != null && args.length > 0) {
-            // http://stackoverflow.com/questions/1523595/programmatically-configure-jettys-logger
-            System.setProperty("org.mortbay.log.class", "ca.simplegames.micro.utils.JettyLogger");
+    if (args != null && args.length > 0) {
+      // Set: org.eclipse.jetty.LEVEL to DEBUG
 
-            String path = args[0];
-            int port = args.length > 1 ? Integer.parseInt(args[1]) : 8080;
-            String envPort = System.getenv(PORT);
+      String path = args[0];
+      int port = args.length > 1 ? Integer.parseInt(args[1]) : 8080;
+      String envPort = System.getenv(PORT);
 
-            if (envPort != null && !envPort.isEmpty()) { // env port overrides user commands
-                try {
-                    port = Integer.parseInt(envPort);
-                } catch (NumberFormatException dontcare) {
-                    //
-                }
-            }
-
-            try {
-
-                SelectChannelConnector connector = new SelectChannelConnector();
-                // connector.setHost("127.0.0.1"); //the loneliest number
-                connector.setPort(port);
-                connector.setThreadPool(new QueuedThreadPool(20));
-                connector.setMaxIdleTime(1000 * 60 * 60); // this will make debugging easier
-
-                Server server = new Server(port);
-                if (new File("jetty.xml").exists()) {
-                    XmlConfiguration configuration = new XmlConfiguration(new FileInputStream("jetty.xml"));
-                    configuration.configure(server);
-                    log.info("jetty config detected ...");
-                }
-                server.setConnectors(new Connector[]{connector});
-                WebAppContext webApp = new WebAppContext(path, "/");
-
-                ServletHandler handler = new ServletHandler();
-                webApp.setServletHandler(handler);
-                webApp.setParentLoaderPriority(true);
-
-                server.addHandler(webApp);
-                server.start();
-                server.join();
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        } else {
-            log.error("You must specify the path to the directory containing the Micro web application.");
+      if (envPort != null && !envPort.isEmpty()) { // env port overrides user commands
+        try {
+          port = Integer.parseInt(envPort);
+        } catch (NumberFormatException dontcare) {
+          //
         }
+      }
+
+      try {
+
+        Server server = new Server(new QueuedThreadPool(DEFAULT_MAX_THREADS));
+        ServerConnector connector = new ServerConnector(server);
+        connector.setPort(port);
+        connector.setIdleTimeout(1000 * 60 * 60);
+        connector.setSoLingerTime(-1);
+
+        if (new File("jetty.xml").exists()) {
+          XmlConfiguration configuration = new XmlConfiguration(new FileInputStream("jetty.xml"));
+          configuration.configure(server);
+          log.info("jetty config detected ...");
+        }
+        server.setConnectors(new Connector[]{connector});
+        WebAppContext webApp = new WebAppContext(path, "/");
+
+        ServletHandler handler = new ServletHandler();
+        webApp.setServletHandler(handler);
+        webApp.setParentLoaderPriority(true);
+
+        server.setHandler(webApp);
+        server.start();
+        server.join();
+      } catch (Exception e) {
+        e.printStackTrace();
+      }
+    } else {
+      log.error("You must specify the path to the directory containing the Micro web application.");
     }
+  }
 }
